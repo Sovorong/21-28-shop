d75c566821231e1e6559ec0330a2c06cbee95258
Sovorong <qhthr417@naver.com>
2021-11-24 14:41:35 +0900

    first commit

---
+++ passport/naver-strategy.js
@@ -0,0 +1,44 @@
+const NaverStrategy = require('passport-naver').Strategy
+const { createSnsUser, existUser, changeUser } = require('../models/auth')
+
+const cb = async (accessToken, refreshToken, profile, done) => {
+	try {
+		// console.log('==================')
+		// console.log(accessToken)
+		// console.log(refreshToken)
+		// console.log(profile)
+		// console.log('==================')
+		let user = { userid: profile.id, accessToken }
+		let userSns = { accessToken, refreshToken, provider: 'NA', snsid: profile.id, 
+			snsName: profile._json.name || null,
+			displayName: profile.displayName || null,
+			profileURL: profile._json.profile_image || null,
+			email: profile._json.email || null,
+		}
+		let { success, idx, status } = await existUser('userid', user.userid)
+		if(success) {
+			if(status === '0') {
+				const { success } = await changeUser(idx, { status: '3' })
+				const { success: success2 } = await changeUser(idx, { status: '3' }, 'users_sns')
+				if(success && success2) user.idx = idx
+				else done('Error')
+			}
+		}
+		else {
+			let { idx: id } = await createSnsUser(user, userSns)
+			user.idx = id
+		}
+		done(null, user)
+	}
+	catch(err) {
+		done(err)
+	}
+}
+
+const naverStrategy = new NaverStrategy({
+	clientID: process.env.NAVER_KEY,
+	clientSecret: process.env.NAVER_SALT,
+	callbackURL: '/auth/naver/cb'
+}, cb)
+
+module.exports = passport => passport.use(naverStrategy)
